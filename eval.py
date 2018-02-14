60da696145051da7dc7750eaf8f80ba0319f8165
bilan <bilan@cip.ifi.lmu.de>
2018-02-14 13:21:34 +0100

    first experiments

--- eval.py
+++ eval.py
@@ -17,7 +17,7 @@ from utils import torch_utils, scorer, constant, helper
 from utils.vocab import Vocab
 
 parser = argparse.ArgumentParser()
-parser.add_argument('model_dir', type=str, help='Directory of the model.')
+parser.add_argument('--model_dir', type=str, help='Directory of the model.', default="saved_models/00")
 parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
 parser.add_argument('--data_dir', type=str, default='dataset/tacred')
 parser.add_argument('--dataset', type=str, default='test', help="Evaluate on dev or test.")
