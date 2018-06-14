154f1593f2f11c6e6491cfc99382daaad24c0dd5
bilan <bilan@cip.ifi.lmu.de>
2018-06-14 22:36:27 +0200

    minor changes

--- runner.py
+++ runner.py
@@ -164,18 +164,15 @@ parser.add_argument(
 
 parser.add_argument('--info', type=str, default='', help='Optional info for the experiment.')
 
-# TODO: careful with this one
-# this is still WIP, we want to set random seed for all files
+# We want to set random seed for all files
 # so instead set the random seed in the global_random_seed.py file
-# TODO: after all this still doesn't seem to work!!!
 parser.add_argument('--seed', type=int, default=RANDOM_SEED)
-
 parser.add_argument('--cuda', type=bool, default=torch.cuda.is_available())
 parser.add_argument('--cpu', action='store_true', help='Ignore CUDA.')
 
 args = parser.parse_args()
 
-# TODO: write the seed into a file and let it be read from there in all files
+# write the seed into a file and let it be read from there in all files
 with open('global_random_seed.py', 'w') as the_file:
     the_file.write('RANDOM_SEED = '+str(args.seed))
 
