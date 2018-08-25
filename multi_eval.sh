c8834f6557206a95d653f5b23ce72b119642818a
bilan <bilan@cip.ifi.lmu.de>
2018-08-25 11:52:14 +0200

    added average of 10 random runs in results

--- multi_eval.sh
+++ multi_eval.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-python eval.py --model_dir saved_models/21
-python eval.py --model_dir saved_models/22
-python eval.py --model_dir saved_models/23
-python eval.py --model_dir saved_models/24
+python eval.py --model_dir saved_models/aaa1 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa2 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa3 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa4 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa5 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa6 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa7 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa8 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa9 --model checkpoint_epoch_60.pt
+python eval.py --model_dir saved_models/aaa10 --model checkpoint_epoch_60.pt
